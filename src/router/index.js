5df6dd761268f7b54e6bdd15a3ee12f778dcb24b
sugarfree3625 <sugarfree3625@gmail.com>
2026-05-21 19:26:02 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../views/Home.vue';
 import DashboardPage from '../views/DashboardPage.vue';
 import ProfilePage from '../views/ProfilePage.vue';
 import MessagesPage from '../views/MessagesPage.vue';
+import AdminPanel from '../components/AdminPanel.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/dashboard', component: DashboardPage },
   { path: '/profile', component: ProfilePage },
-  { path: '/messages', component: MessagesPage }
+  { path: '/messages', component: MessagesPage },
+  { path: '/admin', component: AdminPanel }
 ];
 
 const router = createRouter({
