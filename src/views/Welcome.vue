fcbe238233674c64764b7a56ff4efd290ad03d33
sugarfree3625 <sugarfree3625@gmail.com>
2026-05-25 14:27:23 +0300

    Create Welcome.vue

---
+++ src/views/Welcome.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container" style="text-align:center;padding:80px 20px">
+    <h1>Добро пожаловать!</h1>
+    <p>Вы успешно зарегистрировались.</p>
+    <router-link to="/dashboard" class="btn btn-p">Перейти в дашборд</router-link>
+  </div>
+</template>
