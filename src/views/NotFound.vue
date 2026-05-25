1fd4323e97dc6ac108b57c58b5b023880563255e
sugarfree3625 <sugarfree3625@gmail.com>
2026-05-25 14:27:01 +0300

    Create NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container" style="text-align:center;padding:80px 20px">
+    <h1 style="font-size:4rem">404</h1>
+    <p>Страница не найдена</p>
+    <router-link to="/" class="btn btn-p">На главную</router-link>
+  </div>
+</template>
