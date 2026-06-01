37973fe781909ab5b4f8ae92477f66397d993b91
sugarfree3625 <sugarfree3625@gmail.com>
2026-06-01 21:53:24 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,29 @@
-// Очистка старого кеша и отключение Service Worker
+const CACHE = 'engclub-v3';
+
+// Очистка старого кеша
 self.addEventListener('install', e => {
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(['/', '/index.html', '/manifest.json']);
+    })
+  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      );
+    })
   );
   e.waitUntil(clients.claim());
 });
 
+// Кеширование запросов
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
