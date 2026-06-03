0a84b2c6829f951331cf6831b82e72595fe6efca
sugarfree3625 <sugarfree3625@gmail.com>
2026-06-04 02:36:46 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,22 @@
   <div id="page-home">
     <HeroSection />
     <LandingSections />
+    
+    <!-- 🔥 ВИДЖЕТЫ (только на главной) -->
+    <OnlineWidget />
+    <AIAssistant />
   </div>
 </template>
 
 <script>
 import HeroSection from '../components/HeroSection.vue';
 import LandingSections from '../components/LandingSections.vue';
+import OnlineWidget from '../components/OnlineWidget.vue';
+import AIAssistant from '../components/AIAssistant.vue';
 
 export default {
   name: 'Home',
-  components: { HeroSection, LandingSections },
+  components: { HeroSection, LandingSections, OnlineWidget, AIAssistant },
   props: ['settings']
 };
 </script>
